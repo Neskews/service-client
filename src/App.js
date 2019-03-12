9a5b7205d6d052888873fb97857876a583ed49b2
Lukas Höpfner <lukas@etabus.de>
2019-03-13 00:28:44 +0100

    some grommet steps (?)
    not convinced yet

--- src/App.js
+++ src/App.js
@@ -1,74 +1,52 @@
-// import React, { Component } from 'react';
-// import PeopleList from './components/People';
+import React, { Component } from 'react';
+import { Box, Grid, Grommet, Text, Heading } from 'grommet';
+import PeopleList from './components/People';
 
-// class App extends Component {
-//   render() {
-//     return <PeopleList />;
-//   }
-// }
+const theme = {
+  global: {
+    font: {
+      family: 'Roboto',
+      size: '14px',
+      height: '20px'
+    }
+  }
+};
 
-// export default App;
-
-import React from 'react';
-import { AppBar, Checkbox, IconButton } from 'react-toolbox';
-import { Layout, NavDrawer, Panel, Sidebar } from 'react-toolbox';
-
-export default class LayoutTest extends React.Component {
-  state = {
-    drawerActive: false,
-    drawerPinned: false,
-    sidebarPinned: false
-  };
-
-  toggleDrawerActive = () => {
-    this.setState({ drawerActive: !this.state.drawerActive });
-  };
-
-  toggleDrawerPinned = () => {
-    this.setState({ drawerPinned: !this.state.drawerPinned });
-  };
-
-  toggleSidebar = () => {
-    this.setState({ sidebarPinned: !this.state.sidebarPinned });
-  };
-
+class App extends Component {
   render() {
     return (
-      <Layout>
-        <NavDrawer
-          active={this.state.drawerActive}
-          pinned={this.state.drawerPinned}
-          permanentAt="xxxl"
-          onOverlayClick={this.toggleDrawerActive}
+      <Grommet theme={theme}>
+        <Grid
+          fill
+          rows={['auto', 'flex']}
+          columns={['auto', 'flex']}
+          gap="small"
+          areas={[
+            { name: 'header', start: [0, 0], end: [1, 0] },
+            { name: 'sidebar', start: [0, 1], end: [0, 1] },
+            { name: 'main', start: [1, 1], end: [1, 1] }
+          ]}
         >
-          <p>Navigation, account switcher, etc. go here.</p>
-        </NavDrawer>
-        <Panel>
-          <AppBar leftIcon="menu" onLeftIconClick={this.toggleDrawerActive} />
-          <div style={{ flex: 1, overflowY: 'auto', padding: '1.8rem' }}>
-            <h1>Main Content</h1>
-            <p>Main content goes here.</p>
-            <Checkbox
-              label="Pin drawer"
-              checked={this.state.drawerPinned}
-              onChange={this.toggleDrawerPinned}
-            />
-            <Checkbox
-              label="Show sidebar"
-              checked={this.state.sidebarPinned}
-              onChange={this.toggleSidebar}
-            />
-          </div>
-        </Panel>
-        <Sidebar pinned={this.state.sidebarPinned} width={5}>
-          <div>
-            <IconButton icon="close" onClick={this.toggleSidebar} />
-          </div>
-          <div style={{ flex: 1 }}>
-            <p>Supplemental content goes here.</p>
-          </div>
-        </Sidebar>
-      </Layout>
+          <Box gridArea="header" background="brand">
+            <Heading size="small">Maybe I need some content</Heading>
+          </Box>
+          <Box gridArea="sidebar" background="light-5">
+            {['First', 'Second', 'Third'].map(label => (
+              <Text>{label}</Text>
+            ))}
+          </Box>
+          <Box
+            gridArea="main"
+            background="light-2"
+            justify="center"
+            align="center"
+          >
+            <PeopleList />
+          </Box>
+        </Grid>
+      </Grommet>
     );
   }
 }
+
+export default App;
